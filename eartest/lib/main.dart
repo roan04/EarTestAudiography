1e89eb90e1327164f4e59ffdec3cbe84b49e6b1c
Khushan Chumun <64229625+roan04@users.noreply.github.com>
2022-03-24 00:26:09 +0400

    24 march 00:25

--- eartest/lib/main.dart
+++ eartest/lib/main.dart
@@ -14,17 +14,26 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center, // set all container in the center
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, //How container are separately
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly, //How container are separately
             children: <Widget>[
               Container(
-                  margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 160.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 160.0),
                   child: Image(image: AssetImage('assets/images/ear_logo.jpg')),
+              ),
+              SizedBox(
+                height: 20.0,
               ),
               Container(
                 child:Text('Hearing Test Welcome')
               ),
+              SizedBox(
+                height: 50.0,
+              ),
               Container(
                   child:Text('An audiometry test on your phone')
+              ),
+              SizedBox(
+                height: 75.0,
               ),
               Container(
                 child: Image(image: AssetImage('assets/images/splashlogo.jpg')),
