db18da4227b92b05d62d5adf253b18e85f40fcc1
Khushan Chumun <64229625+roan04@users.noreply.github.com>
2022-05-02 11:28:33 +0400

    Login page completed

---
+++ eartest/lib/SignUp.dart
@@ -0,0 +1,7 @@
+import 'package:adobe_xd/gradient_xd_transform.dart';
+import 'package:eartest/screen2.dart';
+import 'package:eartest/screen_notification.dart';
+import 'package:eartest/splashscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:audioplayers/audioplayers.dart';
